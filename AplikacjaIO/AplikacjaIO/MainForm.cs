32b68fae4ec6c4b534a89a4c7ba9c0258584e51b
mb305151 <115078322+mb305151@users.noreply.github.com>
2024-06-19 10:57:57 +0200

    Dodano obsługę przycisków START - STOP - PRZERWA

--- AplikacjaIO/AplikacjaIO/MainForm.cs
+++ AplikacjaIO/AplikacjaIO/MainForm.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using AplikacjaIO;
 using AplikacjaIO.Klasy;
 
@@ -5,73 +8,113 @@ namespace Projekt_IO_3
 {
     public partial class MainForm : Form
     {
+        private System.Windows.Forms.Timer _timer;
+        private TimeSpan _elapsedTime;
+        private DateTime _startTime;
+        private bool _isRunning;
+        private bool _isPaused;
+
         public MainForm()
         {
             InitializeComponent();
+            _elapsedTime = TimeSpan.Zero;
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Interval = 1000; // 1 sekunda
+            _timer.Tick += Timer_Tick;
             //LoadProjectNames();
         }
 
         //private void LoadProjectNames()
         //{
         //    DataBase dataBase = new DataBase();
-            
-        //        List<string> projectNames = dataBase.GetAllNames();
-
-        //        foreach (string projectName in projectNames)
-        //        {
-        //            ListViewItem item = new ListViewItem(projectName);
-        //            // Jeúli masz inne kolumny do wype≥nienia, uøyj item.SubItems.Add
-        //            ProjectList.Items.Add(item);
-        //        }         
+        //    List<string> projectNames = dataBase.GetAllNames();
+        //    foreach (string projectName in projectNames)
+        //    {
+        //        ListViewItem item = new ListViewItem(projectName);
+        //        // Je≈õli masz inne kolumny do wype≈Çnienia, u≈ºyj item.SubItems.Add
+        //        ProjectList.Items.Add(item);
+        //    }         
         //}
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
         }
 
         private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void Start_btn_Click_1(object sender, EventArgs e)
         {
+            if (!_isRunning)
+            {
+                _isRunning = true;
+                _isPaused = false;
+                _startTime = DateTime.Now;
+                _timer.Start();
+            }
+            else if (_isPaused)
+            {
+                _isPaused = false;
+                _startTime = DateTime.Now - _elapsedTime;
+                _timer.Start();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (_isRunning)
+            {
+                _timer.Stop();
+                _isRunning = false;
+                _isPaused = false; // Zresetowanie flagi pauzy
+                _elapsedTime = TimeSpan.Zero;
+                textBox1.Text = "00:00:00";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_isRunning && !_isPaused)
+            {
+                _timer.Stop();
+                _isPaused = true;
+                _elapsedTime = DateTime.Now - _startTime; // Zapisanie up≈ÇywajƒÖcego czasu przy pauzie
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _elapsedTime = DateTime.Now - _startTime;
+            textBox1.Text = _elapsedTime.ToString(@"hh\:mm\:ss");
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void nowyProjektToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
         }
 
         private void edycjaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-
         }
 
         private void NewProject_menu_Click(object sender, EventArgs e)
