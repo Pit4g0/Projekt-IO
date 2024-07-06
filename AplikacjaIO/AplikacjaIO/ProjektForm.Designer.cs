d266aff48c75e285ddc05d9c98710c4542785f69
Pit4g0 <115000009+Pit4g0@users.noreply.github.com>
2024-07-06 10:32:36 +0200

    v3
    v3

--- AplikacjaIO/AplikacjaIO/ProjektForm.Designer.cs
+++ AplikacjaIO/AplikacjaIO/ProjektForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AplikacjaIO
 {
-    partial class DataForm
+    partial class ProjektForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,22 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            NewData_tb = new TextBox();
+            ProjektNazwa_tb = new TextBox();
             Cancel_btn = new Button();
             AddData_btn = new Button();
+            label1 = new Label();
+            KategorieList_comboBox = new ComboBox();
+            label2 = new Label();
             SuspendLayout();
             // 
-            // NewData_tb
+            // ProjektNazwa_tb
             // 
-            NewData_tb.Location = new Point(206, 120);
-            NewData_tb.Name = "NewData_tb";
-            NewData_tb.Size = new Size(154, 25);
-            NewData_tb.TabIndex = 0;
-            NewData_tb.TextChanged += NewData_tb_TextChanged;
+            ProjektNazwa_tb.Location = new Point(206, 97);
+            ProjektNazwa_tb.Name = "ProjektNazwa_tb";
+            ProjektNazwa_tb.Size = new Size(154, 25);
+            ProjektNazwa_tb.TabIndex = 0;
+            ProjektNazwa_tb.TextChanged += NewData_tb_TextChanged;
             // 
             // Cancel_btn
             // 
-            Cancel_btn.Location = new Point(206, 153);
+            Cancel_btn.Location = new Point(206, 187);
             Cancel_btn.Name = "Cancel_btn";
             Cancel_btn.Size = new Size(60, 23);
             Cancel_btn.TabIndex = 1;
@@ -53,7 +56,7 @@
             // 
             // AddData_btn
             // 
-            AddData_btn.Location = new Point(284, 153);
+            AddData_btn.Location = new Point(284, 187);
             AddData_btn.Name = "AddData_btn";
             AddData_btn.Size = new Size(76, 23);
             AddData_btn.TabIndex = 2;
@@ -61,15 +64,45 @@
             AddData_btn.UseVisualStyleBackColor = true;
             AddData_btn.Click += AddData_btn_Click;
             // 
-            // DataForm
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(206, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 17);
+            label1.TabIndex = 3;
+            label1.Text = "Nazwa Projektu";
+            // 
+            // KategorieList_comboBox
+            // 
+            KategorieList_comboBox.FormattingEnabled = true;
+            KategorieList_comboBox.Location = new Point(206, 156);
+            KategorieList_comboBox.Name = "KategorieList_comboBox";
+            KategorieList_comboBox.Size = new Size(154, 25);
+            KategorieList_comboBox.TabIndex = 4;
+            KategorieList_comboBox.SelectedIndexChanged += Projekty_comboBox_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(206, 136);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 17);
+            label2.TabIndex = 5;
+            label2.Text = "Kategoria Projektu";
+            // 
+            // ProjektForm
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(577, 281);
+            Controls.Add(label2);
+            Controls.Add(KategorieList_comboBox);
+            Controls.Add(label1);
             Controls.Add(AddData_btn);
             Controls.Add(Cancel_btn);
-            Controls.Add(NewData_tb);
-            Name = "DataForm";
+            Controls.Add(ProjektNazwa_tb);
+            Name = "ProjektForm";
             Text = "DataForm";
             ResumeLayout(false);
             PerformLayout();
@@ -77,8 +110,11 @@
 
         #endregion
 
-        private TextBox NewData_tb;
+        private TextBox ProjektNazwa_tb;
         private Button Cancel_btn;
         private Button AddData_btn;
+        private Label label1;
+        private ComboBox KategorieList_comboBox;
+        private Label label2;
     }
 }
---
+++ AplikacjaIO/AplikacjaIO/ZadanieForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AplikacjaIO
+{
+    partial class ZadanieForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Cancel_btn = new Button();
+            AddZadanie_btn = new Button();
+            ZadanieNazwa = new TextBox();
+            ProjektList_comboBox = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // Cancel_btn
+            // 
+            Cancel_btn.Location = new Point(221, 221);
+            Cancel_btn.Name = "Cancel_btn";
+            Cancel_btn.Size = new Size(75, 23);
+            Cancel_btn.TabIndex = 0;
+            Cancel_btn.Text = "Anuluj";
+            Cancel_btn.UseVisualStyleBackColor = true;
+            Cancel_btn.Click += Cancel_btn_Click;
+            // 
+            // AddZadanie_btn
+            // 
+            AddZadanie_btn.Location = new Point(302, 221);
+            AddZadanie_btn.Name = "AddZadanie_btn";
+            AddZadanie_btn.Size = new Size(75, 23);
+            AddZadanie_btn.TabIndex = 1;
+            AddZadanie_btn.Text = "Zatwierdź";
+            AddZadanie_btn.UseVisualStyleBackColor = true;
+            AddZadanie_btn.Click += AddZadanie_btn_Click;
+            // 
+            // ZadanieNazwa
+            // 
+            ZadanieNazwa.Location = new Point(221, 137);
+            ZadanieNazwa.Name = "ZadanieNazwa";
+            ZadanieNazwa.Size = new Size(156, 25);
+            ZadanieNazwa.TabIndex = 2;
+            // 
+            // ProjektList_comboBox
+            // 
+            ProjektList_comboBox.FormattingEnabled = true;
+            ProjektList_comboBox.Location = new Point(221, 190);
+            ProjektList_comboBox.Name = "ProjektList_comboBox";
+            ProjektList_comboBox.Size = new Size(156, 25);
+            ProjektList_comboBox.TabIndex = 3;
+            ProjektList_comboBox.SelectedIndexChanged += AddZadanie_comboBox_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(221, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 17);
+            label1.TabIndex = 4;
+            label1.Text = "Nazwa Zadania";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(221, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 17);
+            label2.TabIndex = 5;
+            label2.Text = "Projekt Nadrzędny";
+            // 
+            // ZadanieForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(610, 351);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(ProjektList_comboBox);
+            Controls.Add(ZadanieNazwa);
+            Controls.Add(AddZadanie_btn);
+            Controls.Add(Cancel_btn);
+            Name = "ZadanieForm";
+            Text = "ZadanieForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Cancel_btn;
+        private Button AddZadanie_btn;
+        private TextBox ZadanieNazwa;
+        private ComboBox ProjektList_comboBox;
+        private Label label1;
+        private Label label2;
+    }
+}
